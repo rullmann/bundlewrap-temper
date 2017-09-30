1ed5b25e8d3820615fc8b0078f514a5aa600d976
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:15 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -1,57 +1,57 @@
 directories = {
-    "/opt/usb-thermometer": {
-        "mode": "0755",
+    '/opt/usb-thermometer': {
+        'mode': '0755',
     },
 }
 
 git_deploy = {
-    "/opt/usb-thermometer": {
-        'repo': "https://github.com/petervojtek/usb-thermometer.git",
-        'rev': "master",
+    '/opt/usb-thermometer': {
+        'repo': 'https://github.com/petervojtek/usb-thermometer.git',
+        'rev': 'master',
         'needs': [
-            "directory:/opt/usb-thermometer",
+            'directory:/opt/usb-thermometer',
         ],
     },
 }
 
 files = {
     '/opt/usb-thermometer/make-temper.sh': {
-        'source': "make_temper.sh",
-        'mode': "0755",
-        'content_type': "mako",
+        'source': 'make_temper.sh',
+        'mode': '0755',
+        'content_type': 'mako',
         'needs': [
-            "git_deploy:/opt/usb-thermometer",
+            'git_deploy:/opt/usb-thermometer',
         ],
         'triggers': [
-            "action:temper_make",
+            'action:temper_make',
         ],
     },
     '/etc/udev/rules.d/99-tempsensor.rules': {
-        'source': "99-tempsensor.rules",
-        'mode': "0644",
+        'source': '99-tempsensor.rules',
+        'mode': '0644',
     },
 }
 
 actions = {
     'temper_make': {
-        'command': "/opt/usb-thermometer/make-temper.sh",
+        'command': '/opt/usb-thermometer/make-temper.sh',
         'triggered': True,
     },
 }
 
-if node.has_bundle("collectd"):
+if node.has_bundle('collectd'):
     files['/etc/collectd.d/temper.conf'] = {
-        'source': "collectd.conf",
-        'mode': "0640",
+        'source': 'collectd.conf',
+        'mode': '0640',
         'triggers': [
-            "svc_systemd:collectd:restart",
+            'svc_systemd:collectd:restart',
         ],
     }
 
     files['/opt/usb-thermometer/collectd.sh'] = {
-        'source': "collectd.sh",
-        'mode': "0755",
+        'source': 'collectd.sh',
+        'mode': '0755',
         'triggers': [
-            "svc_systemd:collectd:restart",
+            'svc_systemd:collectd:restart',
         ],
     }
